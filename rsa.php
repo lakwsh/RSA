1195f1be1cd87a8977f77b2a95d8b85c99b246f3
lakwsh <uitony@126.com>
2018-01-01 00:51:39 +0800

    first commit

--- rsa.php
+++ rsa.php
@@ -1,30 +1,10 @@
 <?php
 class rsa{
-	// 公钥
-	private $public_key='';
-	// 私钥
-	private $private_key='';
-	// 公密钥资源
-	private $public_key_resource='';
-	// 私密钥资源
-	private $private_key_resource='';
-	/**
-	 * 架构函数
-	 * @param [string] $public_key_file  [公密钥文件地址]
-	 * @param [string] $private_key_file [私密钥文件地址]
-	 */
-	public function __construct($public_key_file,$private_key_file){
-		try{
-			if(!file_exists($public_key_file) || !file_exists($private_key_file)) throw new Exception('key file no exists');
-			if(false==($this->public_key=file_get_contents($public_key_file)) || false==($this->private_key=file_get_contents($private_key_file))) throw new Exception('read key file fail');
-			if(!($this->public_key_resource=openssl_pkey_get_public($this->public_key)) or !($this->private_key_resource=openssl_pkey_get_private($this->private_key))) throw new Exception('public key or private key no usable');
-		}catch(Exception $e){
-			exit($e->getMessage());
-		}
-	}
+	private $public_key;
+	private $private_key;
 	/**
 	 * 生成公私钥
-	 * @return array|false
+	 * @return boolean
 	 */
 	public function create_key(){
 		$config=array(
@@ -35,8 +15,10 @@ class rsa{
 		$res=openssl_pkey_new($config);
 		if($res==false) return false;
 		openssl_pkey_export($res,$private_key,null,$config);
-		$public_key=openssl_pkey_get_details($res);
-		return array('public_key'=>$public_key["key"],'private_key'=>$private_key);
+		$public_key=openssl_pkey_get_details($res)["key"];
+		$this->public_key=$public_key;
+		$this->private_key=$private_key;
+		return true;
 	}
 	/**
 	 * 私钥加密
@@ -44,7 +26,7 @@ class rsa{
 	 * @return string
 	 */
 	public function private_encrypt(string $code){
-		openssl_private_encrypt($code,$output,$this->private_key_resource);
+		openssl_private_encrypt($code,$output,$this->private_key);
 		return base64_encode($output);
 	}
 	/**
@@ -53,7 +35,7 @@ class rsa{
 	 * @return string
 	 */
 	public function public_decrypt(string $code){
-		openssl_public_decrypt(base64_decode($code),$output,$this->public_key_resource);
+		openssl_public_decrypt(base64_decode($code),$output,$this->public_key);
 		return $output;
 	}
 	/**
@@ -62,7 +44,7 @@ class rsa{
 	 * @return string
 	 */
 	public function public_encrypt(string $code){
-		openssl_public_encrypt($code,$output,$this->public_key_resource);
+		openssl_public_encrypt($code,$output,$this->public_key);
 		return base64_encode($output);
 	}
 	/**
@@ -71,7 +53,18 @@ class rsa{
 	 * @return string
 	 */
 	public function private_decrypt(string $code){
-		openssl_private_decrypt(base64_decode($code),$output,$this->private_key_resource);
+		openssl_private_decrypt(base64_decode($code),$output,$this->private_key);
 		return $output;
+	}
+	/**
+	 * 公钥/私钥格式化
+	 * @param $key [公钥/私钥]
+	 * @param $public [是否为公钥]
+	 * @return string
+	 */
+	function format_key(string $key,bool $public=true){
+		$pem=chunk_split($key,64,PHP_EOL);
+		if($public) return '-----BEGIN PUBLIC KEY-----'.PHP_EOL.$pem.'-----END PUBLIC KEY-----';
+		else return '-----BEGIN PRIVATE KEY-----'.PHP_EOL.$pem.'-----END PRIVATE KEY-----';
 	}
 }
